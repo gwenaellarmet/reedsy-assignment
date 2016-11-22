b7cb0ac09ca9b7e9fbfc8093c05157c289f92a22
Gwenael Larmet <gwenael@soluteo.com>
2016-11-22 11:38:41 -0500

    Add related books in book view

--- client/app/book/book.controller.js
+++ client/app/book/book.controller.js
@@ -15,7 +15,9 @@ angular.module('reedsyAssignmentApp')
         $http.get('/api/books').success(function(books) {
             $scope.books = books;
             $scope.book = _.filter(books, function(o) { return o.id == bookid; })[0];
-            console.log('$scope.book', $scope.book);
+            $scope.relatedBooks = _.sample(_.filter(books, function(o) { 
+                return o.genre.category == $scope.book.genre.category || o.genre.name == $scope.book.genre.name; 
+            }), 3);
         });
     }
   });
